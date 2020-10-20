38bc7e1bd7e07867120d124e2b03209bad161b34
kevinlhoste <kevin.lhoste@gmail.com>
2020-10-20 18:30:59 +0200

    Updated MPU9250 example with raw value from uint8 buffer

--- Unit Tests/MPU9250_rawData/MPU9250_rawData.ino
+++ Unit Tests/MPU9250_rawData/MPU9250_rawData.ino
@@ -4,25 +4,20 @@
 
 MPU9250_asukiaaa mySensor(0x69);
 float aX, aY, aZ, aSqrt, gX, gY, gZ, mDirection, mX, mY, mZ;
+float accelRange=16.0;
+float gyroRange=2000.0;
+uint8_t* accelBuf;
+uint8_t* gyroBuf;
+uint8_t* magBuf;
+
+int dataMode=0; //0 : raw int | 1 : Float 
 
 void setup() {
   Serial.begin(115200);
-  while(!Serial);
   Serial.println("started");
-
-#ifdef _ESP32_HAL_I2C_H_ // For ESP32
-  Wire.begin(SDA_PIN, SCL_PIN);
-  mySensor.setWire(&Wire);
-#endif
-
   mySensor.beginAccel();
   mySensor.beginGyro();
   mySensor.beginMag();
-
-  // You can set your own offset for mag values
-  // mySensor.magXOffset = -50;
-  // mySensor.magYOffset = -55;
-  // mySensor.magZOffset = -10;
 }
 
 void loop() {
@@ -35,20 +30,74 @@ void loop() {
   }
 */
   if (mySensor.accelUpdate() == 0) {
-    aX = mySensor.accelX();
-    uint32_t toto = mySensor.accelX();
-    float toto2=float(toto);
-    aY = mySensor.accelY();
-    aZ = mySensor.accelZ();
-    aSqrt = mySensor.accelSqrt();
-    Serial.print("accelX: " + String(aX));
-    Serial.println("uintaccelX: " + String(toto));
-    //Serial.println("accelY: " + String(aY));
-    //Serial.println("accelZ: " + String(aZ));
-    //Serial.println("accelSqrt: " + String(aSqrt));
-  } else {
+    if(dataMode==0){
+         accelBuf=mySensor.getAccelBuffer();
+         int16_t v = ((int16_t) accelBuf[0]) << 8 | accelBuf[1];
+         aX=((float) -v) * accelRange / (float) 0x8000; // (float) 0x8000 == 32768.0
+         v = ((int16_t) accelBuf[2]) << 8 | accelBuf[3];
+         aY=((float) -v) * accelRange / (float) 0x8000; // (float) 0x8000 == 32768.0
+         v = ((int16_t) accelBuf[4]) << 8 | accelBuf[5];
+         aZ=((float) -v) * accelRange / (float) 0x8000; // (float) 0x8000 == 32768.0
+         }
+    else if (dataMode==1){
+      aX = mySensor.accelX();
+      aY = mySensor.accelY();
+      aZ = mySensor.accelZ();
+      aSqrt = mySensor.accelSqrt();      
+      }
+      } else {
     Serial.println("Cannod read accel values");
   }
+if (mySensor.gyroUpdate() == 0) {
+    if(dataMode==0){
+         gyroBuf=mySensor.getGyroBuffer();
+          int16_t v = ((int16_t) gyroBuf[0]) << 8 | gyroBuf[1];
+         gX=((float) -v) * gyroRange / (float) 0x8000;
+           v = ((int16_t) gyroBuf[2]) << 8 | gyroBuf[3];
+         gY=((float) -v) * gyroRange / (float) 0x8000;
+          v = ((int16_t) gyroBuf[4]) << 8 | gyroBuf[5];
+         gZ=((float) -v) * gyroRange / (float) 0x8000;
+    }
+    else if (dataMode==1){
+          gX = mySensor.gyroX();
+      gY = mySensor.gyroY();
+      gZ = mySensor.gyroZ(); 
+    }
+      } else {
+    Serial.println("Cannod read gyro values");
+  }
+ if (mySensor.magUpdate() == 0) {
+    if(dataMode==0){
+        magBuf=mySensor.getMagBuffer();
+        mX=(((int16_t) magBuf[0]) << 8) | magBuf[1];
+        mY=(((int16_t) magBuf[2]) << 8) | magBuf[3];
+        mZ=(((int16_t) magBuf[4]) << 8) | magBuf[5];
+    }
+    else if (dataMode==1){
+        mX = mySensor.magX();
+        mY = mySensor.magY();
+        mZ = mySensor.magZ();
+    }
+      } else {
+    Serial.println("Cannod read mag values");
+  }
+
+
+
+      
+  Serial.print("aX: " + String( aX));
+  Serial.print(" aY: " + String( aY));
+  Serial.println(" aZ: " + String( aZ));
+  Serial.print("gX: " + String( gX));
+  Serial.print(" gY: " + String( gY));
+  Serial.println(" gZ: " + String( gZ));
+  Serial.print("mX: " + String( mX));
+  Serial.print(" mY: " + String( mY));
+  Serial.println(" mZ: " + String( mZ));
+   
+  
+   
+
 
  /* if (mySensor.gyroUpdate() == 0) {
     gX = mySensor.gyroX();
