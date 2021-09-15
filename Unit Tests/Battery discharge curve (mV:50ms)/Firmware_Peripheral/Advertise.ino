81e129e89e79d13c879171868b3947b7027aad64
Youssouph Ndao <47628329+Youniesta@users.noreply.github.com>
2021-09-15 21:33:31 +0200

    Add files via upload

---
+++ Unit Tests/Battery discharge curve (mV:50ms)/Firmware_Peripheral/Advertise.ino
@@ -0,0 +1,23 @@
+void startAdv(void)
+{
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+
+
+  Bluefruit.Advertising.addService(ErrorService, PPG86Service, IMUService, hrms);
+  
+  // Include Name
+  Bluefruit.Advertising.addName();
+
+  /* Start Advertising
+     - Enable auto advertising if disconnected
+     - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+     - Timeout for fast mode is 30 seconds
+     - Start(timeout) with timeout = 0 will advertise forever (until connected)
+  */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
+}
