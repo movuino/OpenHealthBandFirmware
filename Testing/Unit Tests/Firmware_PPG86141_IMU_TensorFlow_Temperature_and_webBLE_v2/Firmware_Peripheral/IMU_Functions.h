5f06b1fe9262a200032d77e1a36e9edb146b02d9
Youniesta <youssouph-djilacoune.ndao@edu.ece.fr>
2022-02-24 18:35:41 +0100

    Firware v2 with more LED configuration | Using Direct Ambient to cancel ambient light | Using SNR to correct data saturation

---
+++ Testing/Unit Tests/Firmware_PPG86141_IMU_TensorFlow_Temperature_and_webBLE_v2/Firmware_Peripheral/IMU_Functions.h
@@ -0,0 +1,170 @@
+#include <MPU9250_asukiaaa.h>
+MPU9250_asukiaaa mySensor(0x69);
+
+float aX, aY, aZ, gX, gY, gZ, mX, mY, mZ;
+uint8_t*  accelBuf;
+uint8_t*  gyroBuf;
+uint8_t*  magBuf;
+uint8_t   accScale = 16;
+int   gyroScale = 2000;
+
+float accelRange = 16.0;
+
+uint8_t bufAcc[11];
+uint8_t bufGyro[11];
+uint8_t bufMag[10];
+
+/*Debug Modes*/
+#define debugAcc1
+#define debugGyr
+#define debugMag
+//#define debugAcc2
+
+void configureIMU() {
+
+  Serial.println("####  IMU CONFIG ####");
+  /*Read sensorId*/
+  uint8_t sensorId;
+
+  if (mySensor.readId(&sensorId) == 0) {
+    Serial.println("sensorId: " + String(sensorId));
+    errorIMU = false;
+  }
+  else {
+    Serial.println("Error cannot read sensor ID");
+  }
+  
+  /*Begin IMU*/
+  if (!errorIMU) {
+    mySensor.beginAccel();
+    mySensor.beginGyro();
+    mySensor.beginMag();
+  }
+    Serial.println();
+
+}
+void updateAcc() {
+  if (mySensor.accelUpdate() == 0) {
+    //read sensor
+    accelBuf = mySensor.accelBuff;
+    //read timestamp
+    uint32_t timestamp = millis();
+    bufAcc[3] = (uint8_t)timestamp;
+    bufAcc[2] = (uint8_t)(timestamp >>= 8);
+    bufAcc[1] = (uint8_t)(timestamp >>= 8);
+    bufAcc[0] = (uint8_t)(timestamp >>= 8);
+    bufAcc[4] = 16;
+
+    for (int i = 5; i <= 10; i++) {
+      bufAcc[i] = accelBuf[i - 5];
+      
+#ifdef debugAcc2
+      Serial.print(String(i));
+      Serial.print(":");
+      Serial.print(String(bufAcc[i]));
+      Serial.print(" ");
+#endif
+    }
+    
+    int16_t v = ((int16_t) accelBuf[0]) << 8 | accelBuf[1];
+    aX = ((float) - v) * accelRange / (float) 0x8000; // (float) 0x8000 == 32768.0
+
+    v = ((int16_t) accelBuf[2]) << 8 | accelBuf[3];
+    aY = ((float) - v) * accelRange / (float) 0x8000; // (float) 0x8000 == 32768.0
+
+    v = ((int16_t) accelBuf[4]) << 8 | accelBuf[5];
+    aZ = ((float) - v) * accelRange / (float) 0x8000; // (float) 0x8000 == 32768.0
+    
+#ifdef debugAcc1
+    Serial.println("----- Accelerometer data ----- :");
+    Serial.print(String(aX));
+    Serial.print(" ");
+    Serial.print(String(aY));
+    Serial.print(" ");
+    Serial.print(String(aZ));
+    Serial.println(" ");
+#endif
+
+  } 
+  else {
+    Serial.println("Cannot read accel values");
+  }
+}
+void updateGyro() {
+  if (mySensor.gyroUpdate() == 0) {
+    gyroBuf = mySensor.gyroBuff;
+    uint32_t timestamp = millis();
+    bufGyro[3] = (uint8_t)timestamp;
+    bufGyro[2] = (uint8_t)(timestamp >>= 8);
+    bufGyro[1] = (uint8_t)(timestamp >>= 8);
+    bufGyro[0] = (uint8_t)(timestamp >>= 8);
+    //bufGyro[4] = 2000;
+    bufGyro[4] = 16;
+
+    for (int i = 5; i <= 10; i++) {
+      bufGyro[i] = gyroBuf[i - 5];
+    }
+
+    int16_t v = ((int16_t) gyroBuf[0]) << 8 | gyroBuf[1];
+    gX = ((float) - v) * gyroScale / (float) 0x8000;
+    
+    v = ((int16_t) gyroBuf[2]) << 8 | gyroBuf[3];
+    gY = ((float) - v) * gyroScale / (float) 0x8000;
+    
+    v = ((int16_t) gyroBuf[4]) << 8 | gyroBuf[5];
+    gZ = ((float) - v) * gyroScale / (float) 0x8000;
+
+#ifdef debugGyr
+    Serial.println("----- Gyrometer data ----- :");
+    Serial.print(String(gX));
+    Serial.print(" ");
+    Serial.print(String(gY));
+    Serial.print(" ");
+    Serial.print(String(gZ));
+    Serial.println(" ");
+#endif
+
+  } else {
+    Serial.println("Cannod read accel values");
+  }
+}
+
+void updateMag() {
+  if (mySensor.magUpdate() == 0) {
+    magBuf = mySensor.magBuff;
+    uint32_t timestamp = millis();
+    bufMag[3] = (uint8_t)timestamp;
+    bufMag[2] = (uint8_t)(timestamp >>= 8);
+    bufMag[1] = (uint8_t)(timestamp >>= 8);
+    bufMag[0] = (uint8_t)(timestamp >>= 8);
+    
+    for (int i = 4; i <= 9; i++) {
+      bufMag[i] = magBuf[i - 4];
+          }
+          
+    int16_t v = ((int16_t) magBuf[1]) << 8 | magBuf[0];
+    mX = ((float) v);
+    
+    v = ((int16_t) magBuf[3]) << 8 | magBuf[2];
+    mY = ((float) v);
+    
+    v = ((int16_t) magBuf[5]) << 8 | magBuf[4];
+    mZ = ((float) v);
+    
+#ifdef debugMag
+    Serial.println("----- Magnetometer data ----- :");
+    Serial.print(String(mX));
+    Serial.print(" ");
+    Serial.print(String(mY));
+    Serial.print(" ");
+    Serial.print(String(mZ));
+    Serial.println(" ");
+#endif
+
+
+  } else {
+    Serial.println("Cannod read accel values");
+  }
+        Serial.println();
+
+}
---
+++ Testing/Unit Tests/Firmware_PPG86141_IMU_TensorFlow_Temperature_and_webBLE_v2/Firmware_Peripheral/Max86141_Functions.h
@@ -0,0 +1,292 @@
+
+/*
+ * Intensity LED  : 0 = 0mA | 255 = 124mA (max). Be careful, data from PD can saturate at 524287 if Intensity LED is too high
+ * PGG Sample Rate : 0x00 = 24.995 samples per second | 0x13 = 4096 samples per second (max)
+ * Sample Average : 2, 4, 8, 16, 32, 64, 128 samples (max)
+ * Led Sequence Control : LED1 (1), LED2 (2), LED3 (3), LED1 and LED2 pulsed simultaneously (4),
+   LED1 and LED3 pulsed simultaneously (5), LED2 and LED3 pulsed simultaneously (6), LED1, LED2 and LED3 pulsed simultaneously (7),
+   Pilot on LED1 (8), DIRECT AMBIENT (9), LED4 [external mux control] (10), LED5 [external mux control] (11), LED6 [external mux control] (12)
+ * DIRECT AMBIENT : DA (i.e. normal photodiode measurements)
+ * Sequence Control is up to the configuration you wish (page 14-15 datasheet)
+ * PD: PhotoDiode
+ * 1 LED is RGB or just 1 color
+*/
+
+#include <MAX86141.h>
+#include <SPI.h>
+
+
+//////////// Pointers for PDsLED use to send 4 samples by Bluetooth /////////////
+uint8_t pt_ledSeq1A_PD1_2[12];
+uint8_t pt_ledSeq1A_PD2_2[12];
+uint8_t SNR1_2[4], SNR2_2[4];
+
+/* Sensor Configurations */
+// Sensor composed with 2 PDs and 1 LED //
+#define PDsLED
+
+
+/* Inculde LED configuration */
+int ledMode[10];
+
+#include "Configuration_Sensor.h"
+
+/* Sample Rate taken */
+#define Sample_Rate
+
+/* Pin Definitions  */
+// #define MISO_PIN              19
+// #define MOSI_PIN              18
+// #define SCK_PIN               5
+//#define SS_PIN                19 //(Adafruit)
+#define SS_PIN                10 //(Movuino)
+
+
+/* Global Variables */
+static int spiClk = 1000000; // 8 MHz Maximum
+int cpt1 = 0, cpt2 = 0;
+int interruptPin = 36;
+bool dataReady = false;
+long startTime;
+long samplesTaken = 0;
+int sequences_size = 0;
+float snr_pd1, snr_pd2;
+MAX86141 pulseOx1;
+
+void configurePPG86(void) {
+  Serial.println("####  PPG MAX86141 CONFIG ####");
+  // Configure IO.
+  pinMode(SS_PIN, OUTPUT);
+  digitalWrite(SS_PIN, HIGH);
+
+  //initialise SPI
+  pulseOx1.spi = new SPIClass(SPI);
+  pulseOx1.SS = SS_PIN;
+  Serial.println("Init Device");
+  pulseOx1.spi->begin();
+
+  delay(100);
+  pulseOx1.setDebug(true);
+
+  ///////////////////// Serial Communication //////////////////
+#ifdef SerialTest
+#ifdef PDsLED
+  sequences_size = config(LED_G /*Green LED selected (Sequence 1A, 0-3)*/ | DA /*Direct Ambient (Sequence 2B, 4-9)*/);
+  pulseOx1.initialisation(2/*nb_pds*/, ledMode/*LedMode*/, sequences_size/*Number of sequences*/, 10/*intensity_LEDs*/, 0x00/*sample_average*/, 0xE/*sample_rate*/, 0x3/*pulse width*/, 0x2/*ADC Range= 16uA*/, spiClk);
+#endif
+#endif
+
+  ///////////////////// Bluetooth Communication //////////////////
+#ifdef BleTest
+#ifdef PDsLED
+  sequences_size = config(LED_G /*Green LED selected (Sequence 1A, 0-3)*/ | DA /*Direct Ambient (Sequence 2B, 4-9)*/);
+  pulseOx1.initialisation(2/*nb_pds*/, ledMode/*LedMode*/, sequences_size/*Number of sequences*/, 10/*intensity_LEDs*/, 0x00/*sample_average*/, 0xE/*sample_rate*/, 0x3/*pulse width*/, 0x2/*ADC Range= 16uA*/, spiClk);
+#endif
+#endif
+
+  Serial.println("--Read Register-- System Control");
+  Serial.println(pulseOx1.read_reg(0x0D));
+
+  Serial.println("--Read Register-- PART_ID");
+  int part_ID = pulseOx1.read_reg(0xFF);
+  Serial.println(part_ID);
+
+  Serial.println("--Read Temp-- 0x40");
+  pulseOx1.write_reg(0x40, 0xFF);
+
+  Serial.println(pulseOx1.read_reg(0x41));
+
+  if (part_ID == 36) {
+    Serial.println("MAX86140 connection succeeded !");
+    errorPPG86 = false;
+  }
+  else if (part_ID == 37) {
+    Serial.println("MAX86141 connection succeeded !");
+    errorPPG86 = false;
+  }
+  else {
+    Serial.println("Connection failed !");
+  }
+
+  delay(1000);
+
+  pulseOx1.setDebug(false);
+
+  startTime = millis();
+
+  Serial.println();
+
+}
+
+
+void updatePPG86(void) {
+
+  uint8_t intStatus;
+  //Read Status
+  intStatus = pulseOx1.read_reg(REG_INT_STAT_1);
+  bool flagA_full = (intStatus & 0x80) >> 7;
+
+  /////// if there is 8 data in the FIFO ///////
+  if (flagA_full) {
+    samplesTaken = samplesTaken + 2;
+    int fifo_size = pulseOx1.device_data_read1();
+
+    //---------------------------- Serial Communication -------------------------------------//
+#ifdef SerialTest
+#ifdef PDsLED
+    Serial.println("----- PPG data ----- :");
+    Serial.println("Reading all data from PD1: ");
+    for (int i = 0; i < fifo_size / 4; i++) {
+      Serial.println(pulseOx1.tab_ledSeq1A_PD1[i]);
+    }
+
+    Serial.println("Reading all data from PD2: ");
+    for (int i = 0; i < fifo_size / 4; i++) {
+      Serial.println(pulseOx1.tab_ledSeq1A_PD2[i]);
+    }
+
+    free(pulseOx1.tab_ledSeq1A_PD1);
+    free(pulseOx1.tab_ledSeq1A_PD2);
+#endif
+#endif
+
+    //---------------------------- Bluetooth Communication ----------------------------------//
+#ifdef BleTest
+
+#ifdef PDsLED
+    Serial.println("----- PPG data ----- :");
+    for (int i = 0; i < fifo_size / 4; i++) {
+      Serial.println(pulseOx1.tab_ledSeq1A_PD1[i]);
+    }
+
+    for (int i = 0; i < fifo_size / 4; i++) {
+      Serial.println(pulseOx1.tab_ledSeq1A_PD2[i]);
+    }
+
+#ifdef Sample_Rate
+    Serial.print("Sample Rate : Hz[");
+    Serial.print((float)(samplesTaken) / ((millis() - startTime) / 1000.0), 2);
+    Serial.print("]");
+    Serial.println();
+    Serial.println();
+#endif
+
+    ///////// See if direct ambient is affecting the output of ADC (OverFlow) /////////
+    uint8_t InterruptStatus_without_AFull_DataReady = pulseOx1.read_reg(REG_INT_STAT_1) << 2;
+    bool InterruptStatus_with_ALC = pulseOx1.read_reg(REG_INT_STAT_1) >> 8;
+    //Serial.println("############## Reading ALC bit ############ " + String(InterruptStatus_with_ALC));
+
+    if (InterruptStatus_with_ALC == 0) {
+      ///////////// Pointer to send only 2 samples by Bluetooth (PD1) ////////////
+      if ((pulseOx1.tab_ledSeq1A_PD1[0] != 0) && (pulseOx1.tab_ledSeq1A_PD1[1] != 0)) {
+
+        ///////////// Addition data of PD1 in buffer to measure SNR (Signal Noise Ratio) //////////
+        pulseOx1.signalData_ledSeq1A_PD1[cpt1] = pulseOx1.tab_ledSeq1A_PD1[0];
+        pulseOx1.signalData_ledSeq1A_PD1[cpt1 + 1] = pulseOx1.tab_ledSeq1A_PD1[1];
+
+        ///////////// Pointer to send only 2 samples by Bluetooth (PD1) ////////////
+        uint32_t timestamp1 = millis();
+        pt_ledSeq1A_PD1_2[3] = (uint8_t)timestamp1;
+        pt_ledSeq1A_PD1_2[2] = (uint8_t)(timestamp1 >>= 8);
+        pt_ledSeq1A_PD1_2[1] = (uint8_t)(timestamp1 >>= 8);
+        pt_ledSeq1A_PD1_2[0] = (uint8_t)(timestamp1 >>= 8);
+
+
+        pt_ledSeq1A_PD1_2[7] = (uint8_t)pulseOx1.tab_ledSeq1A_PD1[0];
+        pt_ledSeq1A_PD1_2[6] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD1[0] >>= 8);
+        pt_ledSeq1A_PD1_2[5] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD1[0] >>= 8);
+        pt_ledSeq1A_PD1_2[4] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD1[0] >>= 8);
+
+        pt_ledSeq1A_PD1_2[11] = (uint8_t)pulseOx1.tab_ledSeq1A_PD1[1];
+        pt_ledSeq1A_PD1_2[10] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD1[1] >>= 8);
+        pt_ledSeq1A_PD1_2[9] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD1[1] >>= 8);
+        pt_ledSeq1A_PD1_2[8] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD1[1] >>= 8);
+
+        cpt1 += 2;
+        if (cpt1 == SIZE) {
+          Serial.println("SNR (dB): " + String(pulseOx1.signaltonoise(pulseOx1.signalData_ledSeq1A_PD1, SIZE)));
+          snr_pd1 = pulseOx1.signaltonoise(pulseOx1.signalData_ledSeq1A_PD1, SIZE);
+          int var = 0;
+          var = 100 * (pulseOx1.signaltonoise(pulseOx1.signalData_ledSeq1A_PD1, SIZE));
+          if (var < 0) {
+            int a = -100 * var;
+            SNR1_2[3] = (uint8_t)a;
+            SNR1_2[2] = (uint8_t)(a >>= 8);
+            SNR1_2[1] = (uint8_t)(a >>= 8);
+            SNR1_2[0] = (uint8_t)(a >>= 8);
+          }
+          else {
+            SNR1_2[3] = (uint8_t)var;
+            SNR1_2[2] = (uint8_t)(var >>= 8);
+            SNR1_2[1] = (uint8_t)(var >>= 8);
+            SNR1_2[0] = (uint8_t)(var >>= 8);
+          }
+          cpt1 = 0;
+        }
+
+      }
+
+
+      if ((pulseOx1.tab_ledSeq1A_PD2[0] != 0) && (pulseOx1.tab_ledSeq1A_PD2[1] != 0)) {
+
+        ///////////// Addition data of PD2 in buffer to measure SNR (Signal Noise Ratio) //////////
+        pulseOx1.signalData_ledSeq1A_PD2[cpt2] = pulseOx1.tab_ledSeq1A_PD2[0];
+        pulseOx1.signalData_ledSeq1A_PD2[cpt2 + 1] = pulseOx1.tab_ledSeq1A_PD2[1];
+
+        ///////////// Pointer to send only 2 samples by Bluetooth (PD2) ////////////
+        uint32_t timestamp2 = millis();
+        pt_ledSeq1A_PD2_2[3] = (uint8_t)timestamp2;
+        pt_ledSeq1A_PD2_2[2] = (uint8_t)(timestamp2 >>= 8);
+        pt_ledSeq1A_PD2_2[1] = (uint8_t)(timestamp2 >>= 8);
+        pt_ledSeq1A_PD2_2[0] = (uint8_t)(timestamp2 >>= 8);
+
+        pt_ledSeq1A_PD2_2[7] = (uint8_t)pulseOx1.tab_ledSeq1A_PD2[0];
+        pt_ledSeq1A_PD2_2[6] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD2[0] >>= 8);
+        pt_ledSeq1A_PD2_2[5] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD2[0] >>= 8);
+        pt_ledSeq1A_PD2_2[4] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD2[0] >>= 8);
+
+        pt_ledSeq1A_PD2_2[11] = (uint8_t)pulseOx1.tab_ledSeq1A_PD2[1];
+        pt_ledSeq1A_PD2_2[10] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD2[1] >>= 8);
+        pt_ledSeq1A_PD2_2[9] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD2[1] >>= 8);
+        pt_ledSeq1A_PD2_2[8] = (uint8_t)(pulseOx1.tab_ledSeq1A_PD2[1] >>= 8);
+
+        cpt2 += 2;
+        if (cpt2 == SIZE) {
+          Serial.println("SNR (dB): " + String(pulseOx1.signaltonoise(pulseOx1.signalData_ledSeq1A_PD2, SIZE)));
+          snr_pd2 = pulseOx1.signaltonoise(pulseOx1.signalData_ledSeq1A_PD2, SIZE);
+          int var = 0;
+          var = 100 * (pulseOx1.signaltonoise(pulseOx1.signalData_ledSeq1A_PD2, SIZE));
+          if (var < 0) {
+            int a = -100 * var;
+            SNR2_2[3] = (uint8_t)a;
+            SNR2_2[2] = (uint8_t)(a >>= 8);
+            SNR2_2[1] = (uint8_t)(a >>= 8);
+            SNR2_2[0] = (uint8_t)(a >>= 8);
+          }
+          else {
+            SNR2_2[3] = (uint8_t)var;
+            SNR2_2[2] = (uint8_t)(var >>= 8);
+            SNR2_2[1] = (uint8_t)(var >>= 8);
+            SNR2_2[0] = (uint8_t)(var >>= 8);
+          }
+          cpt2 = 0;
+        }
+      }
+
+      ////////// Detect saturation and with SNR set the led intensity to correct the measurement //////////
+      if ((snr_pd1 > 80.0) || (snr_pd2 > 80.0)) {
+        Serial.println("############################ SATURATION #########################");
+        /// Regulate led instensity to 20 ///
+        pulseOx1.setIntensityLed(20, ledMode);
+      }
+
+    }
+    free(pulseOx1.tab_ledSeq1A_PD1);
+    free(pulseOx1.tab_ledSeq1A_PD2);
+#endif
+
+#endif
+  }
+  Serial.println();
+
+}
