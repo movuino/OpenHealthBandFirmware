d625264a134c54b5045170f77804b33bbdc36fc1
Youniesta <youssouph-djilacoune.ndao@edu.ece.fr>
2022-03-31 22:56:57 +0200

    Setting Acc and Gyro to 125 Hz

---
+++ test/unit/MP9250SampleRatecustom_setting/MP9250SampleRatecustom_setting.ino
@@ -0,0 +1,51 @@
+#include "MPU9250.h"
+
+MPU9250 mpu;
+long startTime;
+long samplesTaken = 0;
+
+void setup() {
+    Serial.begin(115200);
+    while(!Serial);
+    Wire.begin();
+
+    MPU9250Setting setting;
+    setting.accel_fs_sel = ACCEL_FS_SEL::A16G;
+    setting.gyro_fs_sel = GYRO_FS_SEL::G2000DPS;
+    setting.mag_output_bits = MAG_OUTPUT_BITS::M16BITS;
+    setting.fifo_sample_rate = FIFO_SAMPLE_RATE::SMPL_125HZ;
+    setting.gyro_fchoice = 0x00;
+    setting.gyro_dlpf_cfg = GYRO_DLPF_CFG::DLPF_41HZ;
+    setting.accel_fchoice = 0x00;
+    setting.accel_dlpf_cfg = ACCEL_DLPF_CFG::DLPF_45HZ;
+
+    if (!mpu.setup(0x69, setting)) {  // change to your own address
+        while (1) {
+            Serial.println("MPU connection failed. Please check your connection with `connection_check` example.");
+            delay(5000);
+        }
+    }
+
+   startTime = millis();
+}
+
+void loop() {
+    if (mpu.update()) {
+        samplesTaken = samplesTaken + 1;
+        Serial.print("Sample Rate : Hz[");
+        Serial.print((float)(samplesTaken) / ((millis() - startTime) / 1000.0), 2);
+        Serial.print("]");
+        Serial.println();
+        Serial.println();
+        print_roll_pitch_yaw();
+    }
+}
+
+void print_roll_pitch_yaw() {
+    Serial.print("Yaw, Pitch, Roll: ");
+    Serial.print(mpu.getYaw(), 2);
+    Serial.print(", ");
+    Serial.print(mpu.getPitch(), 2);
+    Serial.print(", ");
+    Serial.println(mpu.getRoll(), 2);
+}
